22898b47b46b54a3e98854e6eed6d9e553f95b45
zhiqiang <31707195+ccsuzhiqiang@users.noreply.github.com>
2018-01-21 23:50:40 -0500

    Add files via upload

---
+++ index.php
@@ -0,0 +1,19 @@
+<?php
+ 
+$title = "eCtfast.com";
+
+$content = '<h3>Welcome!   eCtfast provides valuable routes & stops information for riding Ctfastrak</h3> 
+          <a href="routemap.php">Service Map</a>
+		  <h5>Route maps offer a whole picture of Ctfastrak service area. Click the link to check it. </h5>
+            <a href="ptest.php">Route & Stops</a> 
+			 <h5>Route & Stops offer information of Ctfastrak stops. Either all stops distribution or stops of specific route could be tracked. </h5>
+             <a href="rlist.php">Real-Time Stop Info</a>  <br/>
+			 <a href="markerbus.php">Real-Time Bus Info</a> <br/>
+		       <h5>Real_Time Info offer real-time information of stops and buses including route sign,arriving time and how long need to wait of apporaching buses.  </h5>
+			 <a href="planner.php">Trip Planner</a><br/>
+		   <h5>Trip planner reconmmends you the nearst Ctfastrak stop and available route for given origin and destination. </h5>';
+           
+			
+
+include 'Template.php';
+?>
