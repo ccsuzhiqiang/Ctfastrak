27b4bc4ce8cc4f3fec65011802574d849a76d526
zhiqiang <31707195+ccsuzhiqiang@users.noreply.github.com>
2018-01-21 23:53:41 -0500

    Add files via upload

---
+++ Template.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+        <title><?php echo $title; ?></title>
+        <link rel="stylesheet" type="text/css" href="Style.css" />
+    </head>
+    <body>
+        <div id="wrapper">
+            <div id="banner">   </div>    
+			
+                
+                 
+            
+           <div id="content_area">
+                <?php echo $content; ?>
+            </div>          
+                         
+            <footer>
+                <p>@Copyright 2018 eCtfast.com;              All rights reserved</p>
+            </footer>
+        </div>
+    </body>
+</html>
