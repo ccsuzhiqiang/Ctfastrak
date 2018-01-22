813f792d2d7adfec43d70eb96f4c78ac0e8a2b8a
zhiqiang <31707195+ccsuzhiqiang@users.noreply.github.com>
2018-01-21 23:52:42 -0500

    Add files via upload

---
+++ rlist.php
@@ -0,0 +1,15 @@
+<?php
+ 
+$title = "route";
+
+$content = '<h4>Please "CLICK" route to check real time stop information </h4>           	  
+             <a href="rt101.php">Route 101-Hartford/New Britain</a> <br/> <br/>
+			 <a href="rt102.php">Route 102-Hartford/New Britain/Bristol</a>  <br/> <br/>
+             <a href="rt121.php">Route 121-MCC/Hartford/UConn Health</a> <br/>  <br/>
+			 <a href="rt128.php">Route 128-Hartford/Westfarms/New Britain via Stanley Street</a>  <br/> <br/>
+			 <a href="rt140.php">Route 140-CCSU Shuttle</a>  <br/> <br/>
+			 <a href="rt144.php">Route 144-Wethersfield/Westfarms via Newington Center and Brittany Farms  </a>  <br/>	 <br/>		 
+			 <a href="rt153.php">Route 153-Flatbush/Copaco via West Hartford Center</a>  <br/> <br/>
+			 <a href="rt161.php">Route 161-St. Francis Hospital/Hartford Hospital via Capitol Avenue</a>  <br/>	 <br/>	
+             <a href="index.php">Home</a>';
+include 'Template.php';
